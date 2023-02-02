aa95178ac79c12c53b96f656aaa0b12b0816bc64
姚鑫 <2713102336@qq.com>
2023-02-02 13:38:38 +0800

    前台模板导入

---
+++ frontend/assets/AppAsset.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace frontend\assets;
+
+use yii\web\AssetBundle;
+
+/**
+ * Main frontend application asset bundle.
+ */
+class AppAsset extends AssetBundle
+{
+    public $basePath = '@webroot';
+    public $baseUrl = '@web/statics';
+    public $css = [
+        //'css/site.css',
+        'css/bootstrap.css',
+        'css/style.css',
+        'css/prettyPhoto.css',
+        'css/owl.carousel.css',
+        'css/owl.theme.css',
+        'css/fontawesome-all.css'
+    ];
+    public $js = [
+        'js/jquery-2.2.3.min.js',
+        'js/bootstrap.js',
+        'js/owl.carousel.js',
+        'js/move-top.js',
+        'js/easing.js',
+        'js/jquery-1.7.2.js',
+        'js/jquery.quicksand.js',
+        'js/script.js',
+        'js/jquery.prettyPhoto.js'
+    ];
+    public $depends = [
+        'yii\web\YiiAsset',
+        'yii\bootstrap\BootstrapAsset',
+    ];
+}
